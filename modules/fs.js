22c979ac81f390a2bf9ff8cf0aa2bf6d230007b2
FernandoChagas <git@github.com:xchagasx/node-boleto.git>
2023-07-27 23:39:42 -0300

    add fs

--- modules/fs.js
+++ modules/fs.js
@@ -9,3 +9,25 @@ fs.mkdir(path.join(__dirname, '/test'), (error) => {
   }
   console.log('File created with sucess')
 })
+
+// criar um arquivo
+fs.writeFile(path.join(__dirname, '/test', 'test.text'), 'hello node!', (error) => {
+  if (error) {
+      return console.log('Error: ', error);
+  }
+      console.log('arquivo criado')
+      //  modificar arquivo
+      fs.appendFile(path.join(__dirname, '/test', 'test.text'), 'hello world!', (error) => {
+        if (error) {
+            return console.log('Error: ', error);
+        }
+            console.log('arquivo modificado')
+      })
+      // ler arquivo
+      fs.readFile(path.join(__dirname, '/test', 'test.text'), 'utf8', (error, data) => {
+        if (error) {
+            return console.log('Error: ', error);
+        }
+            console.log(data)
+      })
+})
